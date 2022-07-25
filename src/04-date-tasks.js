a648c25e0dd0eb8789630190d76fb3f0aaf55c55
Maria Kagermanova <maria.kgm04@gmail.com>
2022-07-25 23:21:25 +0300

    feat: accomplish some date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4 !== 0) return false;
+  if (year % 100 !== 0) return true;
+  if (year % 400 !== 0) return false;
+  return true;
 }
 
 
@@ -73,8 +77,18 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let final = '';
+  const timespan = endDate - startDate;
+  const hours = Math.floor(timespan / 3600000);
+  const minutes = Math.floor((timespan - (hours * 3600000)) / 60000);
+  const seconds = Math.floor((timespan - (hours * 3600000) - (minutes * 60000)) / 1000);
+  const milleseconds = timespan % 1000;
+  final += hours > 9 ? `${hours}:` : `0${hours}:`;
+  final += minutes > 9 ? `${minutes}:` : `0${minutes}:`;
+  final += seconds > 9 ? `${seconds}.` : `0${seconds}.`;
+  final += milleseconds > 9 ? `${milleseconds}` : `00${milleseconds}`;
+  return final;
 }
 
 
