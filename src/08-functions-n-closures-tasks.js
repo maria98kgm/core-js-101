8e8dff6854c159bfcdde03f67ac968e7f5c9c152
Maria Kagermanova <maria.kgm04@gmail.com>
2022-07-26 23:46:17 +0300

    feat: accomplish condition- and function- tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -179,8 +179,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let num = startFrom - 1;
+  return function check() {
+    num += 1;
+    return num;
+  };
 }
 
 
